12d92b90b99afe96497d7c9fb10048c630321ee4
Kimukun <info@kimu.se>
2016-12-02 22:09:50 +0100

    Version 1.0 Indoor release
    Added:
    Weapons
    - AK6 (Black, Desert, Green)
    - AK6B (Black, Desert, Green)
    - AK6GL (Black, Desert, Green)
    - KSP90C
    - MP5K
    - PSG25
    - Robot 35 Mini-Spike (Green, Desert)
    - Robot 57 NLAW
    Uniforms
    - M90 Uniform
    - M90K Uniform
    Vests
    - M90 Platecarrier
    - M90 Platecarrier Lite
    - M90 Platecarrier Special
    - M90K Platecarrier
    - M90K Platecarrier Lite
    - M90K Platecarrier Special
    - Full Ghillie (M90, M90K, M90L)
    Backpacks
    - M90 Kitbag
    - M90K Kitbag
    Headgear
    - M90 Boonie
    - M90 ECH
    - M90 ECH Lite
    - M90K Boonie
    - M90K ECH
    - M90K ECH Lite
    Insignia
    - Swedish Insignia
    Marker
    - Sweden
    Flag
    - Flag (Sweden)
    
    Known issues:
    - Insignia won't show up on the uniforms.
    
    Credits:
    Bamse - Getting the code for Apex weapons
    Skägget - Fixing the medicbag-script
    Vicky - Helping with information and suggestions

---
+++ @SAF2035/mod.cpp
@@ -0,0 +1,7 @@
+name = "Swedish Armed Forces 2035";
+picture = "\saf2035_main\data\ui\saf2035_logo_ca.paa";
+hidePicture = "false";
+hideName = "false";
+description = "Swedish Armed Forces 2035";
+logo = "\saf2035_main\data\ui\swe_icon_ca.paa";
+overview = "Swedish Armed Forces 2035 comes with a wide varaity of units based on the Swedish Armed Forces in a futuristic version. Units comes in regular M90 Woodland and M90K Desert versions. Also Special Forces in Scorpion W2 version.";
